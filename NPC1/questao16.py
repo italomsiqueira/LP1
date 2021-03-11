7045a780392de18588c7f25ca9fa6b78a4285b9a
Italo Siqueira <56133422+italomsiqueira@users.noreply.github.com>
2021-03-10 21:18:11 -0300

    Add files via upload

---
+++ NPC1/questao16.py
@@ -0,0 +1,9 @@
+print("múltiplos de 2 em um intervalo decrescente de 100 até 40")
+for num in range(100, 39, -1):
+    if num % 2 == 0:
+        print(num)
+
+print("\nPotências de 10 em um intervalo de 2 a 12")
+for num in range(2, 13):
+    if num % 10 == 0:
+        print(num)
