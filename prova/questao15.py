1295bf8ddfc81a038a77510657749d7f394e1c38
Italo Siqueira <56133422+italomsiqueira@users.noreply.github.com>
2021-03-10 21:21:57 -0300

    Delete prova directory

--- prova/questao15.py
+++
@@ -1,9 +0,0 @@
-print("Imprimindo Ímpares do 1000 a 43")
-for num in range(1000, 42, -1):
-    if num % 2 != 0:
-        print(num)
-
-print("\nImprimindo Pares do 2 ao 100")
-for num in range(2, 101):
-    if num % 2 == 0:
-        print(num)
--- prova/questao2.py
+++
@@ -1,4 +0,0 @@
-def conta(a, b):
-    return (a*4)/2/(b**2)
-
-print(conta(2, 3))
